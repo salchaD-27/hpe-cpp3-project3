dce7f1b7d0033d3b44d984af81007a3a81347346
lazyG <lazygolith@gmail.com>
2026-06-02 15:52:17 +0530

    completed task 2 - dynamic key value replacement

--- scripts/simulator.py
+++ scripts/simulator.py
@@ -20,7 +20,8 @@ LOG_FILES = [
     # ("hpcmlog.json",            "hpcmlog.jsonl"),
     # ("monitoring_service.json", "monitoring_service.jsonl"),
     # ("syslog.json",             "syslog.jsonl"),
-    ("sample_alerting_task3.json",             "sample_alerting_task3.jsonl"),
+    # ("sample_alerting_task3.json",             "sample_alerting_task3.jsonl"),
+    ("dynamic_kv_ip_logs.json",             "dynamic_kv_ip_logs.jsonl"),
 ]
 
 # ---------------------------------------------------------------------------
