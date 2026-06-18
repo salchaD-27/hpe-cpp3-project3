67fbb22d2b0b9ba793cfb3c0eb7449460b4ba43a
lazyG <lazygolith@gmail.com>
2026-06-18 14:31:24 +0530

    structured workaround to simulate the same alertmanager alert resolution using vmalert

--- heartbeat.sh
+++ heartbeat.sh
@@ -10,7 +10,7 @@ touch "$LOG_FILE"
 while true; do
   timestamp=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
-  echo "{\"Resource\":{\"service.name\":\"heartbeat\"},\"Body\":\"$NODE_NAME heartbeat\",\"Attributes\":{\"monitoring_services.filename\":\"$LOG_FILE\",\"node\":\"$NODE_NAME\",\"timestamp\":\"$timestamp\",\"event.type\":\"heartbeat\"},\"Severity\":\"INFO\"}" >> "$LOG_FILE"
+  echo "{\"Resource\":{\"service.name\":\"heartbeat_detected\"},\"Body\":\"$NODE_NAME heartbeat detected\",\"Attributes\":{\"monitoring_services.filename\":\"$LOG_FILE\",\"node\":\"$NODE_NAME\",\"timestamp\":\"$timestamp\",\"event.type\":\"heartbeat\"},\"Severity\":\"INFO\"}" >> "$LOG_FILE"
 
   sleep 1
 done
